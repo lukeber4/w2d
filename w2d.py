a306a880ae4920b739bf827d0ac0477f72730232
lukeber4 <lukeber4@gmail.com>
2016-07-07 11:36:41 -0300

    Improved performance

--- w2d.py
+++ w2d.py
@@ -2,47 +2,45 @@
 import sys
 import time
 
-if len(sys.argv) < 2:
-	print "Missing word."
-else:
-	word=sys.argv[1]
-	filename=word+".out"
+l2n = dict(zip("aAeEiIlLoOsStTgGgB","443311110055776688"))
+
+def letterToNumber(l):
+	if l in l2n:
+		l=l2n[l]
+	return l
+
+def changeCase(l):
+	if l.islower():
+		l=l.upper()
+	else:
+		l=l.lower()
+	return l
+
+def word2dictionary(word):
 	dic=set()
 	words=set()
 	dic.add(word)
 	for i in range(0,len(word)):
 		words=set(dic)
 		for w in words:
-			b=""
-			a=""
+			b="" # string before letter
+			a="" # string after letter
+			letter=w[i]
 			for j in range(0,i):
 				b=b+w[j]
 			for j in range(i+1,len(w)):
 				a=a+w[j]
-			l=w[i]
-			if l.islower():
-				dic.add(b+l.upper()+a)
-			else:
-				dic.add(b+l.lower()+a)
-			if l is 'a' or l is 'A':
-				dic.add(b+str(4)+a)
-			else if l is 'e' or l is 'E':
-				dic.add(b+str(3)+a)
-			else if l is 'i' or l is 'I':
-				dic.add(b+str(1)+a)
-			else if l is 'l' or l is 'L':
-				dic.add(b+str(1)+a)
-			else if l is 'o' or l is 'O':
-				dic.add(b+str(0)+a)
-			else if l is 's' or l is 'S':
-				dic.add(b+str(5)+a)
-			else if l is 't' or l is 'T':
-				dic.add(b+str(7)+a)
-			else if l is 'g' or l is 'G':
-				dic.add(b+str(6)+a)
-				dic.add(b+str(9)+a)
-			else if l is 'b' or l is 'B':
-				dic.add(b+str(8)+a)	
+			dic.add(b+changeCase(letter)+a)
+			dic.add(b+letterToNumber(letter)+a)
+	return dic
+
+if len(sys.argv) < 2:
+	print "Missing word. Syntax: python w2d.py word"
+else:
+	dic=set()
+	word=sys.argv[1]
+	dic=word2dictionary(word)
+	filename=word+".out"
 	with open(filename,"w") as f:
 		f.write('\n'.join(dic)+'\n')
 	print "Generated "+str(len(dic))+" combinations. Saved in "+filename
