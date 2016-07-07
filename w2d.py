23f021a8d92a91f32cb789f9827e92e13cb85daf
lukeber4 <lukeber4@gmail.com>
2016-07-07 11:50:22 -0300

    Style fixed

--- w2d.py
+++ w2d.py
@@ -2,45 +2,49 @@
 import sys
 import time
 
-l2n = dict(zip("aAeEiIlLoOsStTgGgB","443311110055776688"))
+l2n = dict(zip("aAeEiIlLoOsStTgGgB", "443311110055776688"))
+
 
 def letterToNumber(l):
-	if l in l2n:
-		l=l2n[l]
-	return l
+    if l in l2n:
+        l = l2n[l]
+    return l
+
 
 def changeCase(l):
-	if l.islower():
-		l=l.upper()
-	else:
-		l=l.lower()
-	return l
+    if l.islower():
+        l = l.upper()
+    else:
+        l = l.lower()
+    return l
+
 
 def word2dictionary(word):
-	dic=set()
-	words=set()
-	dic.add(word)
-	for i in range(0,len(word)):
-		words=set(dic)
-		for w in words:
-			b="" # string before letter
-			a="" # string after letter
-			letter=w[i]
-			for j in range(0,i):
-				b=b+w[j]
-			for j in range(i+1,len(w)):
-				a=a+w[j]
-			dic.add(b+changeCase(letter)+a)
-			dic.add(b+letterToNumber(letter)+a)
-	return dic
+    dic = set()
+    words = set()
+    dic.add(word)
+    for i in range(0, len(word)):
+        words = set(dic)
+        for w in words:
+            b = ""  # String before letter
+            a = ""  # String after letter
+            letter = w[i]
+            for j in range(0, i):
+                b = b + w[j]
+            for j in range(i+1, len(w)):
+                a = a + w[j]
+            dic.add(b + changeCase(letter) + a)
+            dic.add(b + letterToNumber(letter) + a)
+    return dic
+
 
 if len(sys.argv) < 2:
-	print "Missing word. Syntax: python w2d.py word"
+    print "Missing word. Syntax: python w2d.py word"
 else:
-	dic=set()
-	word=sys.argv[1]
-	dic=word2dictionary(word)
-	filename=word+".out"
-	with open(filename,"w") as f:
-		f.write('\n'.join(dic)+'\n')
-	print "Generated "+str(len(dic))+" combinations. Saved in "+filename
+    dic = set()
+    word = sys.argv[1]
+    dic = word2dictionary(word)
+    filename = word + ".out"
+    with open(filename, "w") as f:
+        f.write('\n'.join(dic)+'\n')
+    print "Generated " + str(len(dic)) + " combinations. Saved in " + filename
